d67818872ca2edd7604f60186614c3e4cddb4d51
harsh-kumbhar <harshkumbhar624@gmail.com>
2025-12-08 17:01:30 +0530

    feat: add database seeding commands to build script

--- build.sh
+++ build.sh
@@ -8,3 +8,11 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 python manage.py create_admin
+
+echo "🌱 Seeding Database..."
+python manage.py seed_home_data
+python manage.py seed_workout_data
+python manage.py link_workout_types  # Don't forget this one!
+python manage.py seed_nutrition_data
+python manage.py seed_academics_data
+echo "✅ Seeding Complete."
